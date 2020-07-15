e2d2f1b131e518ae3d83336b074ddbcc64332910
Darylabrador <abrador.daryl@gmail.com>
2020-07-15 07:23:53 +0400

    Adding login / signup backend logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,13 +41,52 @@ exports.getSignup = (req, res, next) => {
  * @throws Will throw an error if one error occursed
  */
 exports.postLogin = (req, res, next) =>{
+    const { email, password } = req.body;
     const errors = validationResult(req);
+    let userFound;
 
     if (!errors.isEmpty()) {
-        const error = new Error('Validation failed');
-        error.statusCode = 422;
-        throw error;
+        return res.status(422).render('auth/login', {
+            path: '/login',
+            title: 'Connexion',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password
+            },
+            validationErrors: errors.array()
+        });
     }
+
+    User.findOne({email: email})
+        .then(user =>{
+            if(!user){
+                const error = new Error('Aucun utilisateur n\'a été trouvé');
+                error.statusCode = 401;
+                throw error;
+            }
+            userFound = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual =>{
+            if(isEqual){
+                req.session.isLoggedIn = true;
+                req.session.user.id    = userFound._id;
+                return req.session.save(err =>{
+                    console.log(err);
+                    res.redirect('/');
+                })
+            }
+            req.flash('error', 'Adresse email ou mot de passe invalide');
+            res.redirect('/login');
+           
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
 }
 
 /**
@@ -58,11 +97,40 @@ exports.postLogin = (req, res, next) =>{
  * @throws Will throw an error if one error occursed
  */
 exports.postSignup = (req, res, next) =>{
+    const { pseudo, email, password, passwordConfirm } = req.body;
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
-        const error = new Error('Validation failed');
-        error.statusCode = 422;
-        throw error;
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            title: 'Inscription',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                pseudo: pseudo,
+                email: email,
+                password: password,
+                passwordConfirm: passwordConfirm
+            },
+            validationErrors: errors.array()
+        });
     }
-}
+
+    bcrypt.hash(password, 12)
+        .then(hashedPwd =>{
+            const user = new User({
+                login: pseudo,
+                email: email,
+                password: hashedPwd
+            });
+            return user.save();
+        })
+        .then(result =>{
+            res.redirect('/login');
+        })
+        .catch(err =>{
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
